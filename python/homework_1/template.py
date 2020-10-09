fc1b943bfe03c0ac1dbd5804392e6165d2f94211
NasTsepyuk <72450328+NasTsepyuk@users.noreply.github.com>
2020-10-09 21:32:13 +0300

    Update template 2
    Изменила задачки t8 и t11, в t6 не очень разобралась, где возникает проблема (по предложенным тестам работает).

--- python/homework_1/template.py
+++ python/homework_1/template.py
@@ -96,13 +96,11 @@ def t8(string):
 
     gh12cdy695m1 -> 695
     """
-    new = ''
-    for i in string:
-        if i.isnumeric():
-            new += ''.join([i])
-        else:
-            new += ' '
-    return int(max(i for i in new.split(' ')))
+    import re
+    s = re.findall(r'\d+', string)
+    for i in range(len(s)):
+        s[i] = int(s[i])
+    return max(s)
 
 
 def t9(number):
@@ -156,6 +154,7 @@ def t11(lst):
     for i in range(len(lst)):
         if sum(x for x in lst[0:i]) == sum(x for x in lst[i + 1::]):
             index = i
+            break
     return index
 
 
