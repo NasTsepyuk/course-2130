4213d97dae3b8dc66dd7ebe44f3f9bc24866a7df
NasTsepyuk <72450328+NasTsepyuk@users.noreply.github.com>
2020-10-09 22:54:09 +0300

    update weather_api

--- python/homework_1/subtask2/weather_api.py
+++ python/homework_1/subtask2/weather_api.py
@@ -1,4 +1,6 @@
 import requests
+from datetime import datetime
+from collections import Counter
 
 
 class WeatherAPI:
@@ -7,9 +9,11 @@ class WeatherAPI:
     api_url = 'https://api.weather.yandex.ru/v2/forecast?lat={lat}&lon={lon}&extra=true'
 
     def __init__(self, api_key):
-        pass
+        self.api_key = api_key
+
 
     def get_current_weather(self, lat, lon):
+
         """
             TODO: return a weather for current day
             {
@@ -21,9 +25,23 @@ class WeatherAPI:
             }
 
         """
-        pass
+        response = requests.get(self.api_url, data={"lat": lat, "lon": lon},
+                                headers={'X-Yandex-API-Key': self.api_key}).json()
+
+        if (response.status_code != 200):
+            return 'Error get current weather from API'
+
+        current_weather = {'time_zone': response['info']['tzinfo']['name'],
+                   'current_time': datetime.fromtimestamp(int(response['now'])).strftime('%Y-%m-%d %H:%M:%S'),
+                   'condition': response['fact']['condition'],
+                   'temp': response['fact']['temp'],
+                   'wind_speed': response['fact']['wind_speed']
+                }
+        return current_weather
+
 
     def get_forecast(self, lat, lon):
+
         """
             TODO: return a forecast for 7 days
             [{
@@ -34,4 +52,18 @@ class WeatherAPI:
                     'sunset': sunset
             }...]
         """
-        pass
+        response = requests.get(self.api_url.format(lat=lat, lon=lon), headers={'X-Yandex-API-Key': self.api_key})
+
+        if response.status_code != 200:
+            return 'Error get current weather from API'
+
+        day_parts = ['night', 'morning', 'day', 'evening']
+        return [{'day': ind,
+                     'temp': sum([day['parts'][p]['temp_avg'] for p in day_parts]) / 4,
+                     'condition': Counter([day['parts'][p]['condition'] for p in day_parts]).most_common(1)[0][0],
+                     'sunrise': day['sunrise'],
+                     'sunset': day['sunset'],
+                     }
+                    for ind, day in enumerate(response['forecasts'])]
+
+
